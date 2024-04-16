2a73f9a6ec07b69a71667f82308696deede95a56
subhajitdas1999 <sd651846@gmail.com>
2024-04-16 23:22:17 +0530

    updtaed return type , added network check in checkWalletConnection

--- app/utils/connectWallet.ts
+++ app/utils/connectWallet.ts
@@ -1,31 +1,31 @@
 import { ethers } from "ethers";
-import { ConnectionErrorCode } from "../defination";
+import { ConnectionErrorCode, ConnectWalletResponse } from "../defination";
 
-const SEPOLIA_NETWORK_ID = "11155111";
-export const connectWallet = async () => {
+const SEPOLIA_NETWORK_ID = 11155111;
+export const connectWallet = async (): Promise<ConnectWalletResponse> => {
   try {
     // Check if MetaMask is installed
     if (window.ethereum) {
-      // Request the user to enable MetaMask
       const provider = new ethers.providers.Web3Provider(window.ethereum);
 
       // Check the network
       const network = await provider.getNetwork();
 
       // If the user is not on the Sepolia network, prompt them to switch
-      if (network.chainId !== parseInt(SEPOLIA_NETWORK_ID)) {
+      if (network.chainId !== SEPOLIA_NETWORK_ID) {
         try {
           await window.ethereum.request({
             method: "wallet_switchEthereumChain",
-            params: [
-              { chainId: ethers.utils.hexlify(parseInt(SEPOLIA_NETWORK_ID)) },
-            ],
+            params: [{ chainId: ethers.utils.hexlify(SEPOLIA_NETWORK_ID) }],
           });
         } catch (switchError: any) {
           if (switchError.code === ConnectionErrorCode.USER_REJECTED_REQUEST) {
             // User rejected the request to switch networks. Exit the function.
-            console.error("User rejected the request to switch networks.");
-            return; // Stop further execution if the network switch is rejected
+            return {
+              success: false,
+              message: "User rejected the request to switch networks",
+              address: null,
+            };
           }
 
           if (switchError.code === ConnectionErrorCode.CHAIN_NOT_ADDED) {
@@ -34,55 +34,74 @@ export const connectWallet = async () => {
                 method: "wallet_addEthereumChain",
                 params: [
                   {
-                    chainId: ethers.utils.hexlify(parseInt(SEPOLIA_NETWORK_ID)),
-                    rpcUrl: "https://rpc.sepolia.org/", // Sepolia RPC URL
+                    chainId: ethers.utils.hexlify(SEPOLIA_NETWORK_ID),
+                    rpcUrl: "https://rpc.sepolia.org/",
                   },
                 ],
               });
             } catch (addError: any) {
-              // If adding the network fails, log the error and exit
-              console.error(
-                `Error adding the Sepolia chain: ${addError.message}`
-              );
-              return; // Stop further execution if adding the network fails
+              // If adding the network fails
+
+              return {
+                success: false,
+                message: "Error adding the Sepolia chain",
+                address: null,
+              };
             }
           } else {
-            console.error(
-              `Error switching to the Sepolia network: ${switchError.message}`
-            );
-            return; // Stop further execution if any other error occurs
+            return {
+              success: false,
+              message: "Error switching to the Sepolia network",
+              address: null,
+            };
           }
         }
       }
 
-      // Request accounts after successfully switching the network
+      // get accounts after successfully switching the network
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
 
-      return address;
-      // Handle the address or signer as needed
+      return {
+        success: true,
+        message: "Wallet connected successfully.",
+        address: address,
+      };
     } else {
-      alert(
-        "Please install MetaMask to use this feature and set it to the Sepolia network!"
-      );
-      return;
+      return {
+        success: false,
+        message:
+          '"Please install MetaMask to use this feature and set it to the Sepolia network!',
+        address: null,
+      };
     }
   } catch (error: any) {
-    console.error(`Error connecting to wallet: ${error.message}`);
-    return;
+    return {
+      success: false,
+      message: "Error connecting to wallet",
+      address: null,
+    };
   }
 };
 
 export const checkWalletConnection = async (): Promise<string | undefined> => {
   if (window.ethereum) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const networkDetails = await provider.getNetwork();
+    //check network
+    if (networkDetails.chainId != SEPOLIA_NETWORK_ID) {
+      return;
+    }
+
     try {
       // Check if we can get accounts without triggering a connection request
       const [account] = await provider.listAccounts();
 
-      return account; // Return the first account if there's any
+      return account;
     } catch (error) {
+      console.log("here");
+
       console.error("Failed to check wallet connection:", error);
     }
   }
